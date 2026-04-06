4ec210b7b1b14dfbaa6bf7ff538103440183ab77
kimzuni <me@kimzuni.com>
2026-04-06 23:27:22 +0900

    tailwindcss 구성

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 
 
@@ -10,6 +11,7 @@ export default defineConfig({
 	plugins: [
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
+		tailwindcss(),
 	],
 	resolve: {
 		alias: [
