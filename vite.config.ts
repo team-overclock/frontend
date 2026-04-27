0316afe9d3dad4cc89d4c1e3a312581a12a0d837
kimzuni <me@kimzuni.com>
2026-04-27 21:09:13 +0900

    chore: vite config 업데이트 - host, port 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,15 +7,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		babel({ presets: [reactCompilerPreset()] }),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: [
-			{ find: "@", replacement: path.resolve(__dirname, "src") },
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [
+			react(),
+			babel({ presets: [reactCompilerPreset()] }),
+			tailwindcss(),
 		],
-	},
+		resolve: {
+			alias: [
+				{ find: "@", replacement: path.resolve(__dirname, "src") },
+			],
+		},
+		server: {
+			host: env.HOST || "localhost",
+			port: Number(env.PORT) || undefined,
+		},
+	};
 });
