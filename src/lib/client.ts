17a80b58621a26d03173a1465e8365d7f9635510
kimzuni <me@kimzuni.com>
2026-05-09 11:01:54 +0900

    feat: axios 기반 request 함수 casing 변환 및 검증 옵션 추가
    - 백엔드 요청 시 snake_case로, 응답 데이터를 camelCase로 자동 변환
    - request 옵션으로 요청/응답 데이터에 대한 zod 기반 검증 옵션 추가

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,8 @@
+import camelcaseKeys from "camelcase-keys";
+import snakecaseKeys from "snakecase-keys";
 import axios from "axios";
-import type { AxiosResponse, AxiosRequestConfig } from "axios";
+import type z from "zod";
+import type { AxiosRequestConfig } from "axios";
 
 import { BACKEND_URL } from "@/shared/env";
 
@@ -20,13 +23,52 @@ export const client = axios.create({
 	},
 });
 
+// 요청 인터셉터를 사용하여 모든 요청 데이터를 snake_case로 변환하여 전송하도록 함
+client.interceptors.request.use(config => {
+	if (config.data) {
+		config.data = snakecaseKeys(config.data, { deep: true });
+	}
+	return config;
+});
 
+// 응답 인터셉터를 사용하여 모든 응답 데이터를 camelCase로 변환하여 수신하도록 함
+client.interceptors.response.use(config => {
+	if (config.data) {
+		config.data = camelcaseKeys(config.data, { deep: true });
+	}
+	return config;
+});
+
+
+
+export interface RequestOptions<
+	T extends z.ZodType = z.ZodType,
+	R extends z.ZodType = z.ZodType,
+	D = unknown,
+> extends AxiosRequestConfig<D> {
+	guard?: {
+		request?: T;
+		response?: R;
+	}
+}
 
 /**
- * `client.request`의 래퍼 함수로, API 요청을 보낼 때 사용할 수 있음
+ * 요청 및 응답 데이터에 대해 검증이 가능한 `client.request`의 래퍼 함수.
  *
- * @param props
+ * {@link RequestOptions}의 `guard` 필드에 Zod 스키마을 포함하여 요청과 응답 데이터에 대한 검증을 수행할 수 있음
  */
-export function request<T = unknown, R = AxiosResponse<T>, D = unknown>(config: AxiosRequestConfig<D>): Promise<R> {
-	return client.request(config);
+export async function request<
+	T extends z.ZodType = z.ZodType,
+	R extends z.ZodType = z.ZodType,
+	D = unknown,
+>({
+	guard,
+	...config
+}: RequestOptions<T, R, D> = {}): Promise<z.infer<R>> {
+	const data = guard?.request ? guard.request.parse(config.data) : config.data;
+	const response = await client.request({
+		...config,
+		data,
+	});
+	return guard?.response ? guard.response.parse(response.data) : response.data;
 }
